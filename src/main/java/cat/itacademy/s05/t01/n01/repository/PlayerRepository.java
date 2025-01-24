58adf1f5c369a2ee1ead25945ad5b8ffeabaa0ae
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 11:46:29 +0100

    PlayerRepository > modify findPlayerByName() by findFirstByName()

--- src/main/java/cat/itacademy/s05/t01/n01/repository/PlayerRepository.java
+++ src/main/java/cat/itacademy/s05/t01/n01/repository/PlayerRepository.java
@@ -8,6 +8,6 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface PlayerRepository extends R2dbcRepository<Player, Integer> {
-    //@Query("SELECT * FROM players WHERE name = :name LIMIT 1")
-    Mono<Player> findPlayerByName(String name);
+    @Query("SELECT * FROM players WHERE name = :name LIMIT 1")
+    Mono<Player> findFirstByName(String name);
 }
